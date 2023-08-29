671900750d8f60328a57547e99bf639ea467198c
ksg0605 <ksg0605@gmail.com>
2023-08-29 20:46:04 +0900

    textfield and deligate pattern

--- TextFieldTestApp/ViewController.swift
+++ TextFieldTestApp/ViewController.swift
@@ -7,13 +7,94 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.delegate = self
+        
+        setup()
+    }
+    
+    func setup() {
+        view.backgroundColor = UIColor.gray
+        
+        textField.keyboardType = UIKeyboardType.emailAddress
+        textField.placeholder = "이메일 입력"
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .next
+        
+        textField.becomeFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+//        textField.resignFirstResponder()
     }
 
-
+    // 텍스트필드의 입력을 시작할때 호출 (시작할지 말지의 여부 허락하는 것)
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    // 시점 -
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+        print("유저가 텍스트필드의 입력을 시작했다.")
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    // 텍스트필드 글자 내용이 (한글자 한글자) 입력되거나 지워질 때 호출이 되고 (허락)
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+//        let maxLength = 10
+//        let currentString: NSString = (textField.text ?? "") as NSString
+//        let newString: NSString = currentString.replacingCharacters(in: range, with: string) as NSString
+//        return newString.length <= maxLength
+        
+        // 입력되고 있는 글자가 "숫자"인 경우 입력을 허용하지 않는 논리
+        if Int(string) != nil {
+            return false
+        } else {
+            //10글자이상 입력되는 것을 막는 코드
+            guard let text = textField.text else { return true }
+            let newLength = text.count + string.count - range.length
+            return newLength <= 10
+        }
+    }
+    
+    // 텍스트필드의 엔터키가 눌러지면 다음 동작을 허락할것인지 말것인지
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function)
+        
+        if textField.text == "" {
+            textField.placeholder = "Type Something!"
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    // 텍스트필드의 입력이 끝날 때 호출 (끝날지 말지를 허락)
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    // 텍스트필드의 입력이 실제 끝났을때 호출 (시점)
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+        print("유저가 텍스트필드의 입력을 끝냈다.")
+    }
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        textField.resignFirstResponder()
+    }
+    
 }
 
